be531803dcf8e12db4a6ca5a37ea97a3008e0525
Mikuts <mikuts2007@gmail.com>
2025-03-31 14:12:21 +0300

    Programmas pirmā versija

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,8 +1,13 @@
+import java.util.Random;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int skaitlis;
+	Random rand = new Random();
+	skaitlis = rand.nextInt(6)+1;
+	System.out.println("Uzkrita skaitlis: "+skaitlis);
+		
 
 	}
 
