e9f31479f8046e98c7ed17817eac9ec7c3fe9a17
Mikuts <mikuts2007@gmail.com>
2025-03-31 14:34:45 +0300

    Pievienota metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 
 public class Kaulins {
-static void mestKaulinu() {
-	int skaitlis;
+static void mestKaulinu(int reizes) {
+	int skaitlis ;
 	Random rand = new Random();
+	for(int i=0; i<reizes; i++) {
 	skaitlis = rand.nextInt(6)+1;
 	System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
 }
 	public static void main(String[] args) {
-	mestKaulinu();
+	mestKaulinu(3);
 		
 
 	}
