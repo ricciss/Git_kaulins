2e062fa402fb13a412922f28588317f2c1d1c436
Mikuts <mikuts2007@gmail.com>
2025-04-01 13:31:05 +0300

    Iespēja ievadīt, cik reizzes mest kauliņu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-	int skaitlis;
+	int skaitlis, reizes;
 	Random rand = new Random();
-	skaitlis = rand.nextInt(6)+1;
-	System.out.println("Uzkrita skaitlis: "+skaitlis);
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Cik reizes mest kauliņu");
+	reizes = scan.nextInt();
+	scan.close();
+	for(int i=0; i<reizes; i++) {
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
+	
 		
 
 	}
