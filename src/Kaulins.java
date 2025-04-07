62215eecf5906da8ba27af422765565fa09a8c08
Mikuts <mikuts2007@gmail.com>
2025-04-07 11:28:45 +0300

    Parāda lielāko skaitli

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -6,12 +6,15 @@ public class Kaulins {
 
 	
 static void mestKaulinu(int reizes) {
-	int skaitlis ;
+	int skaitlis, lielakais=0;
 	Random rand = new Random();
 	for(int i=0; i<reizes; i++) {
 	skaitlis = rand.nextInt(6)+1;
 	System.out.println("Uzkrita skaitlis: "+skaitlis);
+	if(lielakais<skaitlis)
+		lielakais=skaitlis;
 	}
+	System.out.println("Lielākais skaitlis no visiem uzmestajiem: "+lielakais);
 }
 	public static void main(String[] args) {
 Scanner scan = new Scanner(System.in);
